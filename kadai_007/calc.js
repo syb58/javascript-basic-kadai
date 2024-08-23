847c7c1f62f04b5cbb38823dae480d7e260ecae3
syb58 <meronsoda58@gmail.com>
2024-08-23 20:34:12 +0900

    課題再提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,20 +1,14 @@
-let num = 15;
+let num = 1;
 
-if( (num % 3) === 0){
+if( ( num % 15) === 0){
+  console.log("3と5の倍数です")
+}
+else if( (num % 3) === 0){
   console.log("3の倍数です");
 }
-else {
-  console.log(num);
-}
-if( (num % 5) === 0){
+else if( (num % 5) === 0){
   console.log("5の倍数です");
 }
-else{
-  console.log(num);
-}
-if(( num % 3) === 0 && ( num % 5) === 0){
-  console.log("3と5の倍数です");
-} 
-else{
+else {
   console.log(num);
 }
