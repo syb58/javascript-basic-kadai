0eb7805fcdb1d5cd648ca1db0ac2c0869b60eb57
syb58 <meronsoda58@gmail.com>
2024-08-23 18:25:58 +0900

    kadai_007

---
+++ kadai_007/calc.js
@@ -0,0 +1,20 @@
+let num = 15;
+
+if( (num % 3) === 0){
+  console.log("3の倍数です");
+}
+else {
+  console.log(num);
+}
+if( (num % 5) === 0){
+  console.log("5の倍数です");
+}
+else{
+  console.log(num);
+}
+if(( num % 3) === 0 && ( num % 5) === 0){
+  console.log("3と5の倍数です");
+} 
+else{
+  console.log(num);
+}
